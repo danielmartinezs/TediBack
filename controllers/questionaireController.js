5536f3bc14f79ea6b4d238cce8ab028841480181
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-06-01 14:53:57 -0500

    new res

--- controllers/questionaireController.js
+++ controllers/questionaireController.js
@@ -49,6 +49,10 @@ const editQuestion = async (req, res) => {
 
 const addQuestion = async (req, res) => {
     const { idc, pregunta, tipo, respuesta } = req.body;
+    console.log(idc)
+    console.log(pregunta)
+    console.log(tipo)
+    console.log(respuesta)
     if(!idc || !pregunta || !tipo || !respuesta){
         return res.status(400).send({ success: false, message: 'No puedes dejar campos vacíos'})
     }
