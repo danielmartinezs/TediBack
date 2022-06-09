eb451e813b62727808919f80c0c8bdd6429cfb7d
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-06-09 13:56:40 -0500

    updates

--- controllers/graphController.js
+++ controllers/graphController.js
@@ -1,9 +1,10 @@
+const { response } = require('express');
 const bcrypt = require('bcrypt');
 const dbconnect = require('../config/dbConnection.js');
 
 const getDatosGraph = async (req, res) => {
     const { id } = req.params;
-    dbconnect.query('SELECT `cuestionario-alumno`.`fecha`, `cuestionario-alumno`.`puntaje` FROM cuestionario, `cuestionario-alumno`, alumno, `tutor-alumno`, tutor WHERE cuestionario.idCuestionario =  `cuestionario-alumno`.`idCuestionario` AND `cuestionario-alumno`.`idAlumno` = alumno.idAlumno AND alumno.idAlumno = `tutor-alumno`.`idAlumno` AND  `tutor-alumno`.`idTutor` = ? AND cuestionario.idCuestionario = 1', [id], (err, respo) => {
+    dbconnect.query('SELECT `cuestionario-alumno`.`fecha`, `cuestionario-alumno`.`puntaje` FROM cuestionario, `cuestionario-alumno`, alumno, `tutor-alumno`, tutor WHERE cuestionario.idCuestionario =  `cuestionario-alumno`.`idCuestionario` AND `cuestionario-alumno`.`idAlumno` = alumno.idAlumno AND alumno.idAlumno = `tutor-alumno`.`idAlumno` AND tutor.idTutor = ? AND cuestionario.idCuestionario = 1', [id], (err, respo) => {
         if(err)
             console.log(err)
         else{
