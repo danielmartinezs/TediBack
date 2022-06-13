0b92338300adc613fd82e23ef53c335214fe633c
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-06-12 21:42:43 -0500

    display de gráficas

--- controllers/graphController.js
+++ controllers/graphController.js
@@ -2,7 +2,7 @@ const { response } = require('express');
 const bcrypt = require('bcrypt');
 const dbconnect = require('../config/dbConnection.js');
 
-const getDatosGraph = async (req, res) => {
+const getDatosGraphPadre = async (req, res) => {
     const { id } = req.params;
     dbconnect.query('SELECT `cuestionario-alumno`.`fecha`, `cuestionario-alumno`.`puntaje` FROM cuestionario, `cuestionario-alumno`, alumno, `tutor-alumno`, tutor WHERE cuestionario.idCuestionario =  `cuestionario-alumno`.`idCuestionario` AND `cuestionario-alumno`.`idAlumno` = alumno.idAlumno AND alumno.idAlumno = `tutor-alumno`.`idAlumno` AND tutor.idTutor = ? AND cuestionario.idCuestionario = 1', [id], (err, respo) => {
         if(err)
@@ -13,4 +13,15 @@ const getDatosGraph = async (req, res) => {
     })
 }
 
-module.exports = { getDatosGraph }
+const getDatosGraphAdmin = async (req, res) => {
+    const { id } = req.params;
+    dbconnect.query('SELECT fecha, puntaje FROM `cuestionario-alumno` WHERE idAlumno = ?', [id], (err, respo) => {
+        if(err)
+            console.log(err)
+        else{
+            return res.status(200).json(respo)
+        }
+    })
+}
+
+module.exports = { getDatosGraphPadre, getDatosGraphAdmin }
--- routes/graphRoute.js
+++ routes/graphRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Graph = require('../controllers/graphController.js');
 
-router.post('/generareporte/:id', Graph.getDatosGraph)
+router.post('/generareporte/:id', Graph.getDatosGraphPadre)
+
+router.post('/generagraphadmin/:id', Graph.getDatosGraphAdmin)
 
 module.exports = router
