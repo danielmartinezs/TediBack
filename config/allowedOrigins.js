f3e622a8bb2e472cfa7bde79f0f5a45e8256b425
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-07-28 13:01:20 -0500

    cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrgins = [
     'https://tedi-integrador.herokuapp.com/',
     'https://fascinating-crumble-61bdb4.netlify.app/',
+    'https://main--fascinating-crumble-61bdb4.netlify.app/',
     'http://localhost:5000'
 ];
 module.exports = allowedOrgins;
