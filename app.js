0461cbd74b9a9702899425b19f5b1cfff8064a54
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-07-28 14:35:53 -0500

    working?

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use('/reportes', require('./routes/reportRoute.js'));
 app.use('/graphs', require('./routes/graphRoute.js'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World, estoy conectado a la base de datos '+process.env.DB_HOST);
+    res.send('Hello World, estoy conectado a la base de datos '+process.env.DATABASE);
 });
 
 app.listen(process.env.PORT || 5000, function(){
