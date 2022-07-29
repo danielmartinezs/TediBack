11cc9f7a756956622f7fe87ca62431659376072a
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-07-29 00:34:49 -0500

    just in case

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrgins = [
     'https://tedi-integrador.herokuapp.com',
     'https://fascinating-crumble-61bdb4.netlify.app',
     'https://main--fascinating-crumble-61bdb4.netlify.app',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:5000',
 ];
 module.exports = allowedOrgins;
