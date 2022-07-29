0ab2e52cebcdb9f62941e6c02349ba3eadadecdb
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-07-28 21:25:22 -0500

    slashlesss

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrgins = [
     //'https://tedi-integrador.herokuapp.com/',
     'https://tedi-integrador.herokuapp.com',
-    'https://fascinating-crumble-61bdb4.netlify.app/',
-    'https://main--fascinating-crumble-61bdb4.netlify.app/',
+    'https://fascinating-crumble-61bdb4.netlify.app',
+    'https://main--fascinating-crumble-61bdb4.netlify.app',
     'http://localhost:5000'
 ];
 module.exports = allowedOrgins;
