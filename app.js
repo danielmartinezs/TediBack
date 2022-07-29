fb8db7e8012151ea47f4ddf6b11291679942c028
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-07-28 21:32:07 -0500

    up

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors(corsOptions));
 
+//CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 //ROUTES
 app.use('/login', require('./routes/loginRoute.js'));
 app.use('/profiles', require('./routes/userManagementRoute.js'));
@@ -21,15 +30,6 @@ app.use('/graphs', require('./routes/graphRoute.js'));
 
 app.get('/', (req, res) => {
     res.send('Hello World, estoy conectado a la base de datos '+process.env.DATABASE);
-});
-
-//CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
 });
 
 app.listen(process.env.PORT || 5000, function(){
