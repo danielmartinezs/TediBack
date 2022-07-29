cebce00850b3dbe47056965c2d45f359c6f1b65d
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-07-29 00:24:48 -0500

    localhost as well

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,7 @@
 const allowedOrgins = require('./allowedOrigins');
 
 const corsOptions = {
-    origin: 'https://fascinating-crumble-61bdb4.netlify.app',
+    origin: 'https://fascinating-crumble-61bdb4.netlify.app || http://localhost:3000',
     optionsSuccessStatus: 200,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 }
