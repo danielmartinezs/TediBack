82668061652c4be4a00cccd72c54d237d47d957c
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-08-01 22:06:26 -0500

    bye http

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrgins = [
     'https://tediescolar.com',
-    'http://localhost:3000',
-    'http://localhost:5000'
+    'localhost:3000',
+    'localhost:5000'
 ];
 module.exports = allowedOrgins;
