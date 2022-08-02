d018db5466683ee2dce00960d95126d7b74aa0d0
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-08-02 01:03:17 -0500

    without http

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrgins = [
     'https://tediescolar.com',
     'https://fascinating-crumble-61bdb4.netlify.app/',
-    'http://localhost:3000'
+    'localhost:3000'
 ];
 module.exports = allowedOrgins;
