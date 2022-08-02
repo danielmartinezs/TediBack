237ffafb4d72fcc7e20e476919812a1a281a22e3
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-08-01 21:34:27 -0500

    bye heroku

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,5 @@
 const allowedOrgins = [
     'https://tediescolar.com',
-    'https://tedi-integrador.herokuapp.com',
-    'https://fascinating-crumble-61bdb4.netlify.app',
-    'https://main--fascinating-crumble-61bdb4.netlify.app',
     'http://localhost:3000',
     'http://localhost:5000'
 ];
