623e4ec3b6875717c805375404109d547db8892e
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-08-01 20:40:27 -0500

    allowedOrigin update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,9 @@
 const allowedOrgins = [
+    'https://tediescolar.com',
     'https://tedi-integrador.herokuapp.com',
     'https://fascinating-crumble-61bdb4.netlify.app',
     'https://main--fascinating-crumble-61bdb4.netlify.app',
     'http://localhost:3000',
-    'http://localhost:5000',
-    'u559107794_tedi',
-    'https://auth-db765.hstgr.io/index.php?route=/database/structure&server=1&db=u559107794_tedi'
+    'http://localhost:5000'
 ];
 module.exports = allowedOrgins;
