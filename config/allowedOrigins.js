97e635bfe2c6fe2af59973e65d5b8001088bffde
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2022-08-01 20:38:43 -0500

    new db pointer

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,5 +4,7 @@ const allowedOrgins = [
     'https://main--fascinating-crumble-61bdb4.netlify.app',
     'http://localhost:3000',
     'http://localhost:5000',
+    'u559107794_tedi',
+    'https://auth-db765.hstgr.io/index.php?route=/database/structure&server=1&db=u559107794_tedi'
 ];
 module.exports = allowedOrgins;
