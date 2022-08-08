f11184204ac30339f7165e47636c556315fa3f89
GermanLopezFdz <germanlopezfernandez.glf@gmail.com>
2022-08-07 21:38:29 -0500

    Changed deprecated route and allowed url for origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrgins = [
     'https://tediescolar.com',
     'https://fascinating-crumble-61bdb4.netlify.app/',
+    '*',
+    'https://tediescolar.xyz',
+    'http://tediescolar.xyz',
     'http://localhost:3000'
 ];
 module.exports = allowedOrgins;
